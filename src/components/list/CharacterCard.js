948533d662d22777ee856b5bf2ce5227119b2b57
MariaRipley <roblesb.maria@gmail.com>
2023-07-01 20:34:19 +0200

    Install react DOM

--- src/components/list/CharacterCard.js
+++ src/components/list/CharacterCard.js
@@ -2,15 +2,17 @@ import "../../styles/character.scss";
 
 function CharacterCard({ character }) {
   return (
-    <article className="characterCard">
-      <img
-        className="characterCard__img"
-        src={character.image}
-        alt="Imagen del personaje"
-      />
-      <h2 className="characterCard__name">{character.name}</h2>
-      <p className="characterCard__species">{character.species}</p>
-    </article>
+    <a href={"/character/" + character.id}>
+      <article className="characterCard">
+        <img
+          className="characterCard__img"
+          src={character.image}
+          alt="Imagen del personaje"
+        />
+        <h2 className="characterCard__name">{character.name}</h2>
+        <p className="characterCard__species">{character.species}</p>
+      </article>
+    </a>
   );
 }
 
